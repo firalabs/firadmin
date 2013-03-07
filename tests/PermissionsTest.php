5263d15e5837cfa774902d9b2cca3e8ce28fb3cb
Maxime Beaudoin <maxime.beaudoin@nurun.com>
2013-03-07 13:24:29 -0500

    spacing fix

--- tests/PermissionsTest.php
+++ tests/PermissionsTest.php
@@ -14,73 +14,75 @@ use Firalabs\Firadmin\Repository\Eloquent\UserRepository as UserRepositoryInterf
  */
 class PermissionsTest extends PHPUnit_Framework_TestCase
 {
-	
+
 	/**
 	 * Teardown
 	 */
-    public function tearDown()
-    {
-        m::close();
-    }
-    
-    /**
-     * Resources we want to tests
-     * 
-     * @var array
-     */
-    protected $_resources = array(
-		'user'
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	/**
+	 * Resources we want to tests
+	 * 
+	 * @var array
+	 */
+	protected $_resources = array (
+		'user' 
 	);
-    
+
 	/**
 	 * Roles we want to tests
 	 * 
 	 * @var array
 	 */
-    protected $_roles = array(
-		'administrator'  => true,
-		'user_administrator' => array('user' => array('create', 'read', 'update', 'delete'))	
+	protected $_roles = array (
+		'administrator' => true, 
+		'user_administrator' => array (
+			'user' => array ('create', 'read', 'update', 'delete') 
+		) 
 	);
-    
+
 	/**
 	 * testAdministratorIsAllowed
 	 */
-    public function testAdministratorIsAllowed()
-    {
-    	//Mock the user repository
-    	$mock = m::mock('Firalabs\Firadmin\Repository\Eloquent\UserRepository');
-        $mock->shouldReceive('getRoles')->once()->andReturn(array('administrator'));
-        
-        //Test permission
-    	$permissions = new Permissions($this->_roles, $this->_resources);
-    	$this->assertTrue($permissions->isAllowed($mock, 'user', 'update'));
-    }
-    
-    /**
+	public function testAdministratorIsAllowed()
+	{
+		//Mock the user repository
+		$mock = m::mock('Firalabs\Firadmin\Repository\Eloquent\UserRepository');
+		$mock->shouldReceive('getRoles')->once()->andReturn(array ('administrator' ));
+		
+		//Test permission
+		$permissions = new Permissions($this->_roles, $this->_resources);
+		$this->assertTrue($permissions->isAllowed($mock, 'user', 'update'));
+	}
+
+	/**
 	 * testGuestIsNotAllowed
 	 */
-    public function testGuestIsNotAllowed()
-    {    	
-    	//Mock the user repository
-    	$mock = m::mock('Firalabs\Firadmin\Repository\Eloquent\UserRepository');
-        $mock->shouldReceive('getRoles')->once()->andReturn(array());
-        
-        //Test permission
-    	$permissions = new Permissions($this->_roles, $this->_resources);
-    	$this->assertFalse($permissions->isAllowed($mock, 'user', 'update'));    	
-    }
-    
-    /**
-     * testUserAdministatorIsAllowed
-     */
-    public function testUserAdministatorIsAllowed()
-    {    	
-    	//Mock the user repository
-    	$mock = m::mock('Firalabs\Firadmin\Repository\Eloquent\UserRepository');
-        $mock->shouldReceive('getRoles')->once()->andReturn(array('user_administrator'));
-        
-        //Test permission
-    	$permissions = new Permissions($this->_roles, $this->_resources);
-    	$this->assertTrue($permissions->isAllowed($mock, 'user', 'update'));    	
-    }
+	public function testGuestIsNotAllowed()
+	{
+		//Mock the user repository
+		$mock = m::mock('Firalabs\Firadmin\Repository\Eloquent\UserRepository');
+		$mock->shouldReceive('getRoles')->once()->andReturn(array ());
+		
+		//Test permission
+		$permissions = new Permissions($this->_roles, $this->_resources);
+		$this->assertFalse($permissions->isAllowed($mock, 'user', 'update'));
+	}
+
+	/**
+	 * testUserAdministatorIsAllowed
+	 */
+	public function testUserAdministatorIsAllowed()
+	{
+		//Mock the user repository
+		$mock = m::mock('Firalabs\Firadmin\Repository\Eloquent\UserRepository');
+		$mock->shouldReceive('getRoles')->once()->andReturn(array ('user_administrator' ));
+		
+		//Test permission
+		$permissions = new Permissions($this->_roles, $this->_resources);
+		$this->assertTrue($permissions->isAllowed($mock, 'user', 'update'));
+	}
 }
